91a5c910bd0298ace5b4b1e25e2e230d9d436004
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-15 15:50:40 +0600

    Add Prisma client to Apollo Server context for database access

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
 import { startStandaloneServer } from "@apollo/server/standalone";
 import createApolloGraphqlServer from "./graphql";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+type TContext = {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+};
+
+const prisma = new PrismaClient();
 
 const main = async () => {
   const server = await createApolloGraphqlServer();
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
+    context: async (): Promise<TContext> => {
+      return { prisma };
+    },
   });
 
   console.log(`🚀  Server ready at: ${url}`);
