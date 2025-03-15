e208d5e0714d1f0fcbe526bb88ef9ceae5e97d4f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-15 15:37:22 +0600

    Refactor Apollo Server initialization to simplify the code structure

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -3,14 +3,10 @@ import { typeDefs } from "./schema";
 import { resolvers } from "./resolver";
 
 const createApolloGraphqlServer = async () => {
-  const graphqlServer = new ApolloServer({
+  return new ApolloServer({
     typeDefs,
     resolvers,
   });
-
-  await graphqlServer.start();
-
-  return graphqlServer;
 };
 
 export default createApolloGraphqlServer;
