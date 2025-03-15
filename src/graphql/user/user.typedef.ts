5ec3ee8e6c70c434fa0d843bd3d967ea91ac06f9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-15 15:38:06 +0600

    Update User type definitions to change dateOfBirth, createdAt, and updatedAt fields to String and comment out doctor relation

--- src/graphql/user/user.typedef.ts
+++ src/graphql/user/user.typedef.ts
@@ -2,21 +2,33 @@ export const typeDefs = `#graphql
     type User {
         id: ID!
         email: String!
-        password: String!
         firstName: String!
         lastName: String
         phoneNumber: String!
         address: String
-        dateOfBirth: DateTime!
+        dateOfBirth: String!
         gender: Gender!
         profilePicture: String
         role: Role!
         isActive: Boolean!
-        createdAt: DateTime!
-        updatedAt: DateTime!
+        createdAt: String!
+        updatedAt: String!
         
         # Relations
         patient: Patient
-        doctor: Doctor
+        # doctor: Doctor
+    }
+
+    enum Gender {
+        MALE
+        FEMALE
+        OTHER
+    }
+
+    enum Role {
+        PATIENT
+        DOCTOR
+        ADMIN
+        SUPER_ADMIN
     }
 `;
