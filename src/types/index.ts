a510ab5416062c583e7cba029a41f9216e83a2bf
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-16 16:10:41 +0600

    Add TypeScript types for JWT user and context management

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+import { Prisma, PrismaClient, ROLE } from "@prisma/client";
+import { DefaultArgs } from "@prisma/client/runtime/library";
+
+export type TJwtUser = {
+  id: string;
+  email: string;
+  role: ROLE;
+};
+
+export type TContext = {
+  prisma: PrismaClient<Prisma.PrismaClientOptions, never, DefaultArgs>;
+  currentUser: TJwtUser | null;
+};
+
+export type TGenericErrorResponse = {
+  statusCode: number;
+  message: string;
+  errorType: string;
+};
