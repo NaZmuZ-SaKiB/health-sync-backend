221d6a4a78cecd210844fd3366f03b1b194826b6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-16 16:11:31 +0600

    Add global error handler to Apollo Server for improved error formatting

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,11 +1,13 @@
 import { ApolloServer } from "@apollo/server";
 import { typeDefs } from "./schema";
 import { resolvers } from "./resolver";
+import globalErrorHandler from "../errors/globalErrorHandler";
 
 const createApolloGraphqlServer = async () => {
   return new ApolloServer({
     typeDefs,
     resolvers,
+    formatError: globalErrorHandler,
   });
 };
 
