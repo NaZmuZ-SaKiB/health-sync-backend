c2a471d94672d7efb3eb90d1937cd833a4fb5830
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-16 16:21:01 +0600

    Add signin mutation to GraphQL user resolvers with validation and token generation

--- src/graphql/user/user.resolvers.ts
+++ src/graphql/user/user.resolvers.ts
@@ -4,7 +4,7 @@ import config from "../../config";
 import AppError from "../../errors/AppError";
 import { TContext } from "../../types";
 import { jwtHelpers } from "../../utils/jwtHelper";
-import { TUserCretaeInput } from "./user.type";
+import { TUserCretaeInput, TUserSigninInput } from "./user.type";
 import bcrypt from "bcrypt";
 
 const queries = {
@@ -40,11 +40,52 @@ const mutations = {
       },
     });
 
-    const token = await jwtHelpers.generateToken(
+    const token = jwtHelpers.generateToken(
       {
         id: user.id,
         email: user.email,
         role: user.role,
+      },
+      config.jwt.jwt_access_token_secret as string,
+      config.jwt.jwt_access_token_expires_in as string
+    );
+
+    return { token };
+  },
+
+  signin: async (_: any, args: TUserSigninInput, { prisma }: TContext) => {
+    const parsedData = await User.validations.signin.parseAsync(args);
+
+    const isUserExist = await prisma.user.findFirst({
+      where: {
+        email: parsedData.email,
+      },
+      select: {
+        id: true,
+        email: true,
+        password: true,
+        role: true,
+      },
+    });
+
+    if (!isUserExist) {
+      throw new AppError(status.UNAUTHORIZED, "Invalid email or password");
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      parsedData.password,
+      isUserExist.password
+    );
+
+    if (!isPasswordMatch) {
+      throw new AppError(status.UNAUTHORIZED, "Invalid email or password");
+    }
+
+    const token = jwtHelpers.generateToken(
+      {
+        id: isUserExist.id,
+        email: isUserExist.email,
+        role: isUserExist.role,
       },
       config.jwt.jwt_access_token_secret as string,
       config.jwt.jwt_access_token_expires_in as string
