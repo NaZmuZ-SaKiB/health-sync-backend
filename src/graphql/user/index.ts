f3b2e1c5223f6b101a7c4dde04bcbab06602620d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-16 16:11:39 +0600

    Add user validation module to GraphQL user exports

--- src/graphql/user/index.ts
+++ src/graphql/user/index.ts
@@ -2,10 +2,14 @@ import { typeDefs } from "./user.typedef";
 import { queries } from "./user.queries";
 import { mutations } from "./user.mutations";
 import { resolvers } from "./user.resolvers";
+import { validations } from "./user.validation";
+
+export * from "./user.type";
 
 export const User = {
   typeDefs,
   queries,
   mutations,
   resolvers,
+  validations,
 };
