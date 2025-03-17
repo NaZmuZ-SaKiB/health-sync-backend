cd8a2f7b7ced6d054d65d2e3b6b8a1ddfa5a633f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-17 16:47:50 +0600

    Add validations to Patient GraphQL module

--- src/graphql/patient/index.ts
+++ src/graphql/patient/index.ts
@@ -2,10 +2,14 @@ import { typeDefs } from "./patient.typedef";
 import { queries } from "./patient.queries";
 import { mutations } from "./patient.mutations";
 import { resolvers } from "./patient.resolvers";
+import { validations } from "./patient.validation";
+
+export * from "./patient.type";
 
 export const Patient = {
   typeDefs,
   queries,
   mutations,
   resolvers,
+  validations,
 };
