12818677d121e0ebb7ed350ca4549dd28a743024
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-18 13:04:55 +0600

    Add seeding logic for Super Admin user in the application startup

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
+import bcrypt from "bcrypt";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import createApolloGraphqlServer from "./graphql";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient, ROLE } from "@prisma/client";
 import { TContext } from "./types";
 import { jwtHelpers } from "./utils/jwtHelper";
 import config from "./config";
@@ -9,6 +10,36 @@ const prisma = new PrismaClient();
 
 const main = async () => {
   const server = await createApolloGraphqlServer();
+
+  // Seed Super Admin
+  const isSuperAdminExist = await prisma.user.findFirst({
+    where: {
+      role: ROLE.SUPER_ADMIN,
+    },
+    select: { id: true },
+  });
+
+  if (!isSuperAdminExist) {
+    try {
+      const hashPassword = await bcrypt.hash(
+        config.super_admin_password as string,
+        Number(config.bcrypt_salt_rounds)
+      );
+
+      await prisma.user.create({
+        data: {
+          email: config.super_admin_email as string,
+          password: hashPassword,
+          role: ROLE.SUPER_ADMIN,
+          firstName: "Super",
+          lastName: "Admin",
+          isActive: true,
+        },
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
