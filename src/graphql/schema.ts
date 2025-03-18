8c10fb1a9085bfd80463d67ce4997ecabda3eedb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-18 13:07:24 +0600

    Add Doctor and Specialty type definitions, queries, and mutations to schema

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -1,9 +1,17 @@
+import { Doctor } from "./doctor";
 import { Patient } from "./patient";
+import { Specialty } from "./specialty";
 import { User } from "./user";
 
 export const typeDefs = `#graphql
   ${User.typeDefs}
   ${Patient.typeDefs}
+  ${Doctor.typeDefs}
+  ${Specialty.typeDefs}
+
+  type GenericSuccessResponse {
+    success: Boolean!
+  }
 
   type Query {
     ${User.queries}
@@ -12,5 +20,7 @@ export const typeDefs = `#graphql
   type Mutation {
     ${User.mutations}
     ${Patient.mutations}
+    ${Doctor.mutations}
+    ${Specialty.mutations}
   }
 `;
