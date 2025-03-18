ea432ff104578c30fcb4649909447c654aa660d6
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-18 16:13:31 +0600

    Set default values for fee in Doctor model and end_time in Doctor schedules

---
+++ prisma/migrations/20250318092922_add_default_value_0_in_fee_doctor_table/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "doctor_schedules" ALTER COLUMN "end_time" SET DEFAULT now() + interval '1 hour';
+
+-- AlterTable
+ALTER TABLE "doctors" ALTER COLUMN "fee" SET DEFAULT 0;
