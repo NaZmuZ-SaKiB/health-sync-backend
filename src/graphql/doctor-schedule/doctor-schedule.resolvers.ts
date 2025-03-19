89b69badf9032e0d60af2f83b2713c41c48152a2
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-19 17:03:18 +0600

    Add updateDoctorSchedule mutation to handle schedule updates and validations

--- src/graphql/doctor-schedule/doctor-schedule.resolvers.ts
+++ src/graphql/doctor-schedule/doctor-schedule.resolvers.ts
@@ -1,8 +1,13 @@
 import { ROLE } from "@prisma/client";
 import { TContext } from "../../types";
 import auth from "../../utils/auth";
-import { TDoctorScheduleCreateInput } from "./doctor-schedule.type";
+import {
+  TDoctorScheduleCreateInput,
+  TDoctorScheduleUpdateInput,
+} from "./doctor-schedule.type";
 import { DoctorSchedule } from ".";
+import AppError from "../../errors/AppError";
+import status from "http-status";
 
 const queries = {};
 
@@ -16,8 +21,57 @@ const mutations = {
 
     const parsedData = await DoctorSchedule.validations.create.parseAsync(args);
 
+    const isDoctorExist = await prisma.doctor.findUnique({
+      where: { id: parsedData.doctorId },
+    });
+
+    if (!isDoctorExist) {
+      throw new AppError(status.NOT_FOUND, "Doctor not found.");
+    }
+
+    const isScheduleExist = await prisma.doctorSchedule.findFirst({
+      where: {
+        doctorId: parsedData.doctorId,
+        day: parsedData.day,
+      },
+    });
+
+    if (isScheduleExist) {
+      throw new AppError(
+        status.BAD_REQUEST,
+        "Doctor schedule already exists for this day. Please update it."
+      );
+    }
+
     await prisma.doctorSchedule.create({
       data: parsedData,
+    });
+
+    return { success: true };
+  },
+
+  updateDoctorSchedule: async (
+    _: any,
+    args: TDoctorScheduleUpdateInput,
+    { prisma, currentUser }: TContext
+  ) => {
+    await auth(prisma, currentUser, [ROLE.DOCTOR]);
+
+    const parsedData = await DoctorSchedule.validations.update.parseAsync(args);
+
+    const isScheduleExist = await prisma.doctorSchedule.findUnique({
+      where: { id: parsedData.scheduleId },
+    });
+
+    if (!isScheduleExist) {
+      throw new AppError(status.NOT_FOUND, "Schedule not found.");
+    }
+
+    const { scheduleId, ...updateData } = parsedData;
+
+    await prisma.doctorSchedule.update({
+      where: { id: parsedData.scheduleId },
+      data: updateData,
     });
 
     return { success: true };
