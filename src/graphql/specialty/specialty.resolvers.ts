d530fa96f53f9cca8395ac9c64c6e528a798f7eb
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-19 13:58:41 +0600

    Add updateSpecialty mutation to handle specialty updates in GraphQL

--- src/graphql/specialty/specialty.resolvers.ts
+++ src/graphql/specialty/specialty.resolvers.ts
@@ -1,7 +1,7 @@
 import { ROLE } from "@prisma/client";
 import { TContext } from "../../types";
 import auth from "../../utils/auth";
-import { TSpecialtyCreate } from "./specialty.type";
+import { TSpecialtyCreate, TSpecialtyUpdate } from "./specialty.type";
 import { Specialty } from ".";
 import AppError from "../../errors/AppError";
 import status from "http-status";
@@ -28,6 +28,34 @@ const mutations = {
     }
 
     await prisma.specialty.create({ data: parsedData });
+
+    return { success: true };
+  },
+
+  updateSpecialty: async (
+    _: any,
+    args: TSpecialtyUpdate,
+    { prisma, currentUser }: TContext
+  ) => {
+    await auth(prisma, currentUser, [ROLE.ADMIN, ROLE.SUPER_ADMIN]);
+
+    const parsedData = await Specialty.validations.update.parseAsync(args);
+
+    const { specialtyId, ...updateData } = parsedData;
+
+    const specialty = await prisma.specialty.findUnique({
+      where: { id: parsedData.specialtyId },
+      select: { id: true },
+    });
+
+    if (!specialty) {
+      throw new AppError(status.NOT_FOUND, "Specialty not found.");
+    }
+
+    await prisma.specialty.update({
+      where: { id: parsedData.specialtyId },
+      data: updateData,
+    });
 
     return { success: true };
   },
