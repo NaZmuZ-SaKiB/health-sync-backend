57cf46a5780cb91c4dcf0dabbe175f33a7ad4225
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-19 17:02:59 +0600

    Add updateDoctorSchedule mutation to GraphQL schema for schedule modifications

--- src/graphql/doctor-schedule/doctor-schedule.mutations.ts
+++ src/graphql/doctor-schedule/doctor-schedule.mutations.ts
@@ -1,8 +1,15 @@
 export const mutations = `#graphql
     createDoctorSchedule(
-            doctorId: String!
-            startTime: String!
-            endTime: String!
-            day: DAY!
-        ): GenericSuccessResponse!
+        doctorId: String!
+        startTime: String!
+        endTime: String!
+        day: DAY!
+    ): GenericSuccessResponse!
+
+    updateDoctorSchedule(
+        scheduleId: String!
+        startTime: String
+        endTime: String
+        isAvailable: Boolean
+    ): GenericSuccessResponse!
 `;
