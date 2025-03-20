7fe028bdf1a196848458cad4091c82d0bb98b96d
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-20 16:33:43 +0600

    Add TAppointmentUpdateInput type for appointment status and notes updates

--- src/graphql/appointment/appointment.type.ts
+++ src/graphql/appointment/appointment.type.ts
@@ -1,3 +1,5 @@
+import { APPOINTMENT_STATUS } from "@prisma/client";
+
 export type TAppointmentCreateInput = {
   input: {
     scheduleId: string;
@@ -8,4 +10,10 @@ export type TAppointmentCreateInput = {
       endTime: string;
     };
   };
+};
+
+export type TAppointmentUpdateInput = {
+  appointmentId: string;
+  status?: APPOINTMENT_STATUS;
+  notes?: string;
 };
