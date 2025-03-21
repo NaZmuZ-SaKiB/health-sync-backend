d870670898865232b2b3ae3bc81bb5c9cf13797b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-21 14:13:12 +0600

    Add payment update input type with paymentId, transactionId, and status fields

--- src/graphql/payment/payment.type.ts
+++ src/graphql/payment/payment.type.ts
@@ -1,1 +1,7 @@
-export type TPaymentUpdateInput = {};
+import { PAYMENT_STATUS } from "@prisma/client";
+
+export type TPaymentUpdateInput = {
+  paymentId: string;
+  transactionId?: string;
+  status?: PAYMENT_STATUS;
+};
