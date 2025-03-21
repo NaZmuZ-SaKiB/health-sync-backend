3964a957f7dee86c775933adcab34dbdebdda720
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-21 16:00:01 +0600

    Add createMedicalReport mutation to handle medical report submissions

---
+++ src/graphql/medical-report/medical-report.mutations.ts
@@ -0,0 +1,11 @@
+export const mutations = `#graphql
+    createMedicalReport(
+        patientId: String!
+        appointmentId: String
+        title: String!
+        reportType: REPORT_TYPE!
+        reportDate: String
+        fileUrl: String
+        notes: String
+    ): GenericSuccessResponse!
+`;
