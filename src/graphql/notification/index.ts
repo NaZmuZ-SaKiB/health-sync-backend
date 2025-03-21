c328db823794c1d5b9aa7d41f4c4389dbddabf50
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-21 16:19:03 +0600

    Add Notification module to GraphQL with type definitions, queries, mutations, and resolvers

---
+++ src/graphql/notification/index.ts
@@ -0,0 +1,11 @@
+import { typeDefs } from "./notification.typedef";
+import { queries } from "./notification.queries";
+import { mutations } from "./notification.mutations";
+import { resolvers } from "./notification.resolvers";
+
+export const Notification = {
+  typeDefs,
+  queries,
+  mutations,
+  resolvers,
+};
