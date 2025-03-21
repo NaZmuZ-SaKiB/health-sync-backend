fee0efb789029afeccc8e41f17fb4c3bbc69547f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-03-21 14:13:23 +0600

    Add validation schema for payment update with paymentId, transactionId, and status

--- src/graphql/payment/payment.validation.ts
+++ src/graphql/payment/payment.validation.ts
@@ -1,1 +1,19 @@
-export const validations = {};
+import { PAYMENT_STATUS } from "@prisma/client";
+import { z } from "zod";
+
+const status = z.enum(
+  Object.values(PAYMENT_STATUS) as [PAYMENT_STATUS, ...PAYMENT_STATUS[]],
+  {
+    errorMap: () => ({
+      message: "Invalid Payment Status.",
+    }),
+  }
+);
+
+const update = z.object({
+  paymentId: z.string().min(1, "Payment ID must be provided."),
+  transactionId: z.string().optional(),
+  status: status.optional(),
+});
+
+export const validations = { update };
