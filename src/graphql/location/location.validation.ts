d7baca1e3a84d99af73900a36c9c3780ef34d69b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-09 20:53:26 +0600

    Add validation schema for creating a location

---
+++ src/graphql/location/location.validation.ts
@@ -0,0 +1,12 @@
+import { z } from "zod";
+
+const create = z.object({
+  name: z.string().nonempty("Name is required"),
+  mapUrl: z.string().url("Invalid URL format for mapUrl"),
+  address: z.string().nonempty("Address is required"),
+  phoneNumber: z.string().nonempty("Phone number is required"),
+  description: z.string().optional(),
+  image: z.string().optional(),
+});
+
+export const validations = { create };
