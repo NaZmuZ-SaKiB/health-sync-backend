1b6f2b46d09b6681126c5006af63fb6716e5559c
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-10 10:45:36 +0600

    Add GraphQL type definition for Image model

---
+++ src/graphql/image/image.typedef.ts
@@ -0,0 +1,19 @@
+export const typeDefs = `#graphql
+    type Image {
+        id: ID!
+        userType: ROLE!
+        name: String!
+        publicId: String!
+        height: Int!
+        width: Int!
+        format: String!
+        url: String!
+        secureUrl: String!
+        thumbnailUrl: String!
+        createdAt: String!
+        updatedAt: String!
+
+        # Relations 
+        user: User!
+    }
+`;
