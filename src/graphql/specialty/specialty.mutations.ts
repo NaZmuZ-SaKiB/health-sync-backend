9a9c690c01ca9ad4cff77ef1cbe38fa50ec9c1c0
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-15 21:41:48 +0600

    feat: Add removeSpecialties mutation to GraphQL schema

--- src/graphql/specialty/specialty.mutations.ts
+++ src/graphql/specialty/specialty.mutations.ts
@@ -2,4 +2,6 @@ export const mutations = `#graphql
     createSpecialty(name: String!, description: String, icon: String): GenericSuccessResponse!
 
     updateSpecialty(specialtyId: String!, name: String, description: String, icon: String): GenericSuccessResponse!
+
+    removeSpecialties(ids: [String!]!): GenericSuccessResponse!
 `;
