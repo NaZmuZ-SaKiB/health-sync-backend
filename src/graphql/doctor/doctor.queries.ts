bf5f890f66b1c86f7772de3b07352d13d75696ee
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-19 16:46:50 +0600

    feat: Update GraphQL queries to include parameters for pagination, search, and filtering in getAllDoctors

--- src/graphql/doctor/doctor.queries.ts
+++ src/graphql/doctor/doctor.queries.ts
@@ -1,1 +1,13 @@
-export const queries = `#graphql`;
+export const queries = `#graphql
+    getAllDoctors(
+        page: String
+        limit: String
+        searchTerm: String
+        sortBy: String
+        sortOrder: String
+        gender: String
+        specialty: String
+        location: String
+        isVerified: String
+    ):DoctorsResponse!
+`;
