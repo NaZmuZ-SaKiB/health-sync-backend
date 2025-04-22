20ab94414493f5a77ee9efbb8e27cfe9f3a8ee0b
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-22 10:31:29 +0600

    feat: Add verifyDoctor mutation to handle doctor verification status updates

--- src/graphql/doctor/doctor.mutations.ts
+++ src/graphql/doctor/doctor.mutations.ts
@@ -2,4 +2,6 @@ export const mutations = `#graphql
     createDoctor(input: UserDoctorCreateInput!): GenericSuccessResponse!
 
     updateDoctor(input: UserDoctorUpdateInput!): GenericSuccessResponse!
+
+    verifyDoctor(doctorId: String!, status: DOCTOR_VERIFICATION_STATUS!): GenericSuccessResponse!
 `;
