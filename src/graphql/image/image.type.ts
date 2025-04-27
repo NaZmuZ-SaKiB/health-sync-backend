fcf9098af0caa13aaab69ec2b193b0af44a57985
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-04-27 23:54:59 +0600

    feat: Refactor TImageCreateInput to include input field for better structure

--- src/graphql/image/image.type.ts
+++ src/graphql/image/image.type.ts
@@ -1,12 +1,14 @@
 export type TImageCreateInput = {
-  name: string;
-  publicId: string;
-  height: number;
-  width: number;
-  format: string;
-  url: string;
-  secureUrl: string;
-  thumbnailUrl: string;
+  input: {
+    name: string;
+    publicId: string;
+    height?: number;
+    width?: number;
+    format: string;
+    url: string;
+    secureUrl: string;
+    thumbnailUrl: string;
+  }[];
 };
 export type TImageUpdateInput = {
   imageId: string;
