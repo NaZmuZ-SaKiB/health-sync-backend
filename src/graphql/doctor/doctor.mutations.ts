112409c387f0250f1f5cde10804fe3763f9785aa
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-01 23:06:11 +0600

    feat: Change return type of updateDoctor mutation to User

--- src/graphql/doctor/doctor.mutations.ts
+++ src/graphql/doctor/doctor.mutations.ts
@@ -1,7 +1,7 @@
 export const mutations = `#graphql
     createDoctor(input: UserDoctorCreateInput!): GenericSuccessResponse!
 
-    updateDoctor(input: UserDoctorUpdateInput!): GenericSuccessResponse!
+    updateDoctor(input: UserDoctorUpdateInput!): User!
 
     verifyDoctor(doctorId: String!, status: DOCTOR_VERIFICATION_STATUS!): GenericSuccessResponse!
 
