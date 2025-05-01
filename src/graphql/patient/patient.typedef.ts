ac9fb218ce5983f7096712f939214fbae6788a4f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-01 23:06:39 +0600

    feat: Remove profilePicture field from UserPatientUpdateInput

--- src/graphql/patient/patient.typedef.ts
+++ src/graphql/patient/patient.typedef.ts
@@ -28,7 +28,6 @@ export const typeDefs = `#graphql
         address: String
         dateOfBirth: String
         gender: GENDER
-        profilePicture: String
         patient: PatientUpdateInput
     }
 `;
