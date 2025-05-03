30efb1fe0776aa15351dca4ef5a8698da765ab4f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-03 22:12:52 +0600

    feat: Update validation for doctor schedule to require scheduleId and improve time checks

--- src/graphql/doctor-schedule/doctor-schedule.validation.ts
+++ src/graphql/doctor-schedule/doctor-schedule.validation.ts
@@ -49,67 +49,74 @@ const create = z
     }
   );
 
-const update = z
-  .object({
-    scheduleId: z.string().min(1, { message: "Schedule ID is required." }), // Required
-    startTime: z
-      .string()
-      .regex(/^\d{2}:\d{2}$/, {
-        message: "Start time must be in HH:MM format.",
-      })
-      .optional(),
-    endTime: z
-      .string()
-      .regex(/^\d{2}:\d{2}$/, { message: "End time must be in HH:MM format." })
-      .optional(),
-    isAvailable: z.boolean().optional(),
-  })
-  .refine(
-    (data) => {
-      // If startTime is provided, endTime must also be provided
-      if (data.startTime && !data.endTime) {
-        return false;
-      }
-      return true;
-    },
-    {
-      message: "End time is required when start time is provided.",
-      path: ["endTime"],
-    }
-  )
-  .refine(
-    (data) => {
-      // If endTime is provided, startTime must also be provided
-      if (data.endTime && !data.startTime) {
-        return false;
-      }
-      return true;
-    },
-    {
-      message: "Start time is required when end time is provided.",
-      path: ["startTime"],
-    }
-  )
-  .refine(
-    (data) => {
-      if (data.startTime && data.endTime) {
-        const start = moment(data.startTime, timeFormat);
-        const end = moment(data.endTime, timeFormat);
-
-        if (!start.isBefore(end)) {
+const update = z.object({
+  ids: z.array(z.string()).min(1, "Min one id is rquired."),
+  data: z
+    .object({
+      startTime: z
+        .string()
+        .regex(/^\d{2}:\d{2}$/, {
+          message: "Start time must be in HH:MM format.",
+        })
+        .optional(),
+      endTime: z
+        .string()
+        .regex(/^\d{2}:\d{2}$/, {
+          message: "End time must be in HH:MM format.",
+        })
+        .optional(),
+      sessionLength: z
+        .number({ invalid_type_error: "Sesson Length must be number." })
+        .optional(),
+      isAvailable: z.boolean().optional(),
+    })
+    .refine(
+      (data) => {
+        // If startTime is provided, endTime must also be provided
+        if (data.startTime && !data.endTime) {
           return false;
         }
-
-        if (end.diff(start, "hours") < 1) {
+        return true;
+      },
+      {
+        message: "End time is required when start time is provided.",
+        path: ["endTime"],
+      }
+    )
+    .refine(
+      (data) => {
+        // If endTime is provided, startTime must also be provided
+        if (data.endTime && !data.startTime) {
           return false;
         }
+        return true;
+      },
+      {
+        message: "Start time is required when end time is provided.",
+        path: ["startTime"],
       }
-      return true;
-    },
-    {
-      message: "End time must be at least 1 hour after start time.",
-      path: ["endTime"],
-    }
-  );
+    )
+    .refine(
+      (data) => {
+        if (data.startTime && data.endTime) {
+          const start = moment(data.startTime, timeFormat);
+          const end = moment(data.endTime, timeFormat);
+
+          if (!start.isBefore(end)) {
+            return false;
+          }
+
+          if (end.diff(start, "hours") < 1) {
+            return false;
+          }
+        }
+        return true;
+      },
+      {
+        message: "End time must be at least 1 hour after start time.",
+        path: ["endTime"],
+      }
+    ),
+});
 
 export const validations = { create, update };
