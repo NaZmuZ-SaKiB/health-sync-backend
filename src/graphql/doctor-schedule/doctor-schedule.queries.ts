4c143a29041473c81317ffbc8d3b997e67fe0d06
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-05 10:23:32 +0600

    feat: Add getDoctorScheduleByDate query for fetching doctor's schedule by date

--- src/graphql/doctor-schedule/doctor-schedule.queries.ts
+++ src/graphql/doctor-schedule/doctor-schedule.queries.ts
@@ -1,3 +1,8 @@
 export const queries = `#graphql
     doctorSchedules(doctorId: String!):[DoctorSchedule!]!
+
+    getDoctorScheduleByDate(
+        doctorId: String!
+        date: String!
+    ): DoctorSchedule
 `;
