fc803ca0808a999b1356c6892fe6803f6f1ad709
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-05 21:58:36 +0600

    feat: Implement getTimeSlotsByDate query to fetch available time slots by doctor and date with validation

--- src/graphql/time-slot/time-slot.resolvers.ts
+++ src/graphql/time-slot/time-slot.resolvers.ts
@@ -1,4 +1,36 @@
-const queries = {};
+import status from "http-status";
+import AppError from "../../errors/AppError";
+import { TContext } from "../../types";
+import moment from "moment";
+
+const queries = {
+  getTimeSlotsByDate: async (
+    _: any,
+    args: { doctorId: string; date: string },
+    { prisma }: TContext
+  ) => {
+    if (!args.date) {
+      throw new AppError(status.BAD_REQUEST, "Date is required");
+    }
+
+    const isValidDate = moment(args.date, "DD-MM-YYYY").isValid();
+    if (!isValidDate) {
+      throw new AppError(
+        status.BAD_REQUEST,
+        "Date Format should be DD-MM-YYYY."
+      );
+    }
+
+    const slots = await prisma.timeSlot.findMany({
+      where: {
+        doctorId: args.doctorId,
+        slotDate: args.date,
+      },
+    });
+
+    return slots;
+  },
+};
 
 const mutations = {};
 
