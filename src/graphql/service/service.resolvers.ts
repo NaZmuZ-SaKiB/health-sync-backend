fce9102216a1a8b59749bbc45b9c492e64f3e1b1
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-12 12:14:24 +0600

    feat: Implement GraphQL queries for retrieving all services and service details

--- src/graphql/service/service.resolvers.ts
+++ src/graphql/service/service.resolvers.ts
@@ -1,12 +1,62 @@
-import { ROLE } from "@prisma/client";
-import { TContext } from "../../types";
+import { Prisma, ROLE } from "@prisma/client";
+import { TContext, TFilters } from "../../types";
 import auth from "../../utils/auth";
 import { TServiceCreateInput } from "./service.type";
 import { Service } from ".";
 import AppError from "../../errors/AppError";
 import status from "http-status";
+import calculatePagination from "../../utils/calculatePagination";
 
-const queries = {};
+const queries = {
+  getAllServices: async (_: any, queries: TFilters, { prisma }: TContext) => {
+    const { page, limit, skip, sortBy, sortOrder } =
+      calculatePagination(queries);
+
+    const conditions: Prisma.ServiceWhereInput = {};
+
+    if (queries?.searchTerm) {
+      conditions.OR = ["name"].map((field) => ({
+        [field]: {
+          contains: queries?.searchTerm,
+          mode: "insensitive",
+        },
+      }));
+    }
+
+    const services = await prisma.service.findMany({
+      where: conditions,
+      orderBy: {
+        [sortBy]: sortOrder,
+      },
+      skip,
+      take: limit,
+    });
+
+    const total = await prisma.service.count({
+      where: conditions,
+    });
+
+    const meta = {
+      page,
+      limit,
+      total,
+    };
+
+    return { services, meta };
+  },
+
+  service: async (_: any, args: { id: string }, { prisma }: TContext) => {
+    if (!args.id) {
+      throw new AppError(status.BAD_REQUEST, "Service ID is required.");
+    }
+
+    const service = await prisma.service.findUnique({
+      where: { id: args.id },
+    });
+
+    return service;
+  },
+};
 
 const mutations = {
   createService: async (
