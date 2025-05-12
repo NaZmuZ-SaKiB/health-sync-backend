ee3ada50a1a7c9d6cb5f009c5e7024cdb7f5ed56
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-12 21:53:07 +0600

    feat: add service update input type

--- src/graphql/service/service.type.ts
+++ src/graphql/service/service.type.ts
@@ -3,3 +3,10 @@ export type TServiceCreateInput = {
   description?: string;
   icon?: string;
 };
+
+export type TServiceUpdateInput = {
+  serviceId: string;
+  name?: string;
+  description?: string;
+  icon?: string;
+};
