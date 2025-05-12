064e6eaf143fbf4e784743231a831b8bc7618710
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-12 21:39:48 +0600

    Update specialty.mutations.ts

--- src/graphql/specialty/specialty.mutations.ts
+++ src/graphql/specialty/specialty.mutations.ts
@@ -1,7 +1,16 @@
 export const mutations = `#graphql
-    createSpecialty(name: String!, description: String, icon: String): SpecialtyCreateUpdateResponse!
+    createSpecialty(
+        name: String!
+        description: String
+        icon: String
+      ): SpecialtyCreateUpdateResponse!
 
-    updateSpecialty(specialtyId: String!, name: String, description: String, icon: String): SpecialtyCreateUpdateResponse!
+    updateSpecialty(
+      specialtyId: String!
+      name: String
+      description: String
+      icon: String
+    ): SpecialtyCreateUpdateResponse!
 
     removeSpecialties(ids: [String!]!): GenericSuccessResponse!
 `;
