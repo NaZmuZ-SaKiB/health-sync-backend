06a1ba3d2376e367de4afad40ff8511d3ee2b781
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-14 20:26:49 +0600

    feat: add resolver for updating service settings

--- src/graphql/service-settings/service-settings.resolvers.ts
+++ src/graphql/service-settings/service-settings.resolvers.ts
@@ -1,5 +1,55 @@
+import { ROLE } from "@prisma/client";
+import { TContext } from "../../types";
+import auth from "../../utils/auth";
+import { TServiceSettingsUpdateInput } from "./service-settings.type";
+import { ServiceSettings } from ".";
+import AppError from "../../errors/AppError";
+import status from "http-status";
+
 const queries = {};
 
-const mutations = {};
+const mutations = {
+  updateServiceSettings: async (
+    _: any,
+    args: TServiceSettingsUpdateInput,
+    { prisma, currentUser }: TContext,
+  ) => {
+    await auth(prisma, currentUser, [ROLE.SUPER_ADMIN, ROLE.ADMIN]);
+
+    const parsedData =
+      await ServiceSettings.validations.update.parseAsync(args);
+
+    const isService = await prisma.service.findUnique({
+      where: { id: parsedData.serviceId },
+      select: { id: true, serviceSettingsId: true },
+    });
+
+    if (!isService) {
+      throw new AppError(status.NOT_FOUND, "Service not found.");
+    }
+
+    const { serviceId, ...updateData } = parsedData;
+
+    if (isService?.serviceSettingsId) {
+      await prisma.serviceSettings.update({
+        where: {
+          id: isService?.serviceSettingsId,
+        },
+        data: updateData,
+      });
+    } else {
+      await prisma.serviceSettings.create({
+        data: {
+          ...updateData,
+          service: {
+            connect: {
+              id: serviceId,
+            },
+          },
+        },
+      });
+    }
+  },
+};
 
 export const resolvers = { queries, mutations };
