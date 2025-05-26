a34df6a8bbfc7d624b5da4ca8e1ee1d3099eb504
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-26 22:30:24 +0600

    feat: define getAllReports query with pagination and sorting parameters

--- src/graphql/medical-report/medical-report.queries.ts
+++ src/graphql/medical-report/medical-report.queries.ts
@@ -1,1 +1,10 @@
-export const queries = `#graphql`;
+export const queries = `#graphql
+  getAllReports(
+    page: String
+    limit: String
+    searchTerm: String
+    sortBy: String
+    sortOrder: String
+    reportType: REPORT_TYPE
+  ) : ReportsResponse!
+`;
