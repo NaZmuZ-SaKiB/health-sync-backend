26c2ab13a1daa5c3d5b374ea4d9c008ebabfa17e
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-26 18:34:06 +0600

    feat: add locationId parameter to getTimeSlotsByDate query

--- src/graphql/time-slot/time-slot.queries.ts
+++ src/graphql/time-slot/time-slot.queries.ts
@@ -2,6 +2,7 @@ export const queries = `#graphql
     getTimeSlotsByDate(
         doctorId: String
         serviceId: String
+        locationId: String
         date: String!
     ): [TimeSlot!]!
 `;
