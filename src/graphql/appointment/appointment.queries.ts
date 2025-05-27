a03fbd7269f53cac0e0b5e4172f73af339ec4763
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-27 20:43:53 +0600

    feat: add 'all' parameter to getAllAppointments query

--- src/graphql/appointment/appointment.queries.ts
+++ src/graphql/appointment/appointment.queries.ts
@@ -7,6 +7,7 @@ export const queries = `#graphql
         sortOrder: String
         status: APPOINTMENT_STATUS
         date: String
+        all: Boolean
     ): AppointmentsResponse!
 
     appointment(id: String!): Appointment!
