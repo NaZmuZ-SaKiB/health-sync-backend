46f8a1b817dc2bafea8268fe7ff31a0639e94dc7
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-05-29 16:19:19 +0600

    feat: add updateUserStatus mutation to user mutations

--- src/graphql/user/user.mutations.ts
+++ src/graphql/user/user.mutations.ts
@@ -11,4 +11,6 @@ export const mutations = `#graphql
     ): AuthResponse!
 
     updateProfilePicture(id: String!): GenericSuccessResponse!
+
+    updateUserStatus(id: String!): GenericSuccessResponse!
 `;
