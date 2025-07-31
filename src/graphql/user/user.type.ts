473f79b5f532f0d1a9e4627c20cc39a165a9d41f
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-07-31 22:14:30 +0600

    feat: add gender field to TUserUpdateInput type

--- src/graphql/user/user.type.ts
+++ src/graphql/user/user.type.ts
@@ -1,7 +1,18 @@
+import { GENDER } from "@prisma/client";
+
 export type TUserCretaeInput = {
   email: String;
   password: String;
   confirmPassword: String;
+};
+
+export type TUserUpdateInput = {
+  firstName?: String;
+  lastName?: String;
+  phoneNumber?: String;
+  address?: String;
+  dateOfBirth?: String;
+  gender?: GENDER;
 };
 
 export type TUserSigninInput = {
