c5542a306950d88711b4d18628fe60bba2e15cd3
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-02 22:11:29 +0600

    Add GraphQL query definition for getAllPayments

--- src/graphql/payment/payment.queries.ts
+++ src/graphql/payment/payment.queries.ts
@@ -1,1 +1,10 @@
-export const queries = `#graphql`;
+export const queries = `#graphql
+  getAllPayments(
+    page: String
+    limit: String
+    searchTerm: String
+    sortBy: String
+    sortOrder: String
+    status: PAYMENT_STATUS
+  ) : PaymentsResponse!
+`;
