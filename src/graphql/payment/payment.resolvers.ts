06748a25af12053460ea980026ffe6978ef80732
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-02 15:48:55 +0600

    feat: implement paymentInit mutation for appointment initialization and payment processing

--- src/graphql/payment/payment.resolvers.ts
+++ src/graphql/payment/payment.resolvers.ts
@@ -1,10 +1,13 @@
-import { PAYMENT_STATUS, ROLE } from "@prisma/client";
+import { APPOINTMENT_STATUS, PAYMENT_STATUS, ROLE } from "@prisma/client";
 import { TContext } from "../../types";
 import auth from "../../utils/auth";
-import { TPaymentUpdateInput } from "./payment.type";
+import { TPaymentInitInput, TPaymentUpdateInput } from "./payment.type";
 import { Payment } from ".";
 import AppError from "../../errors/AppError";
 import status from "http-status";
+import { utils } from "./payment.utils";
+import config from "../../config";
+import axios from "axios";
 
 const queries = {};
 
@@ -12,7 +15,7 @@ const mutations = {
   updatePayment: async (
     _: any,
     args: TPaymentUpdateInput,
-    { prisma, currentUser }: TContext
+    { prisma, currentUser }: TContext,
   ) => {
     await auth(prisma, currentUser, [
       ROLE.DOCTOR,
@@ -45,11 +48,11 @@ const mutations = {
 
     if (
       currentUser?.role === ROLE.DOCTOR &&
-      isPaymentExist.appointment.doctor.userId !== currentUser.id
+      isPaymentExist?.appointment?.doctor?.userId !== currentUser.id
     ) {
       throw new AppError(
         status.FORBIDDEN,
-        "You are not allowed to update this payment"
+        "You are not allowed to update this payment",
       );
     }
 
@@ -75,6 +78,102 @@ const mutations = {
     });
 
     return { success: true };
+  },
+
+  paymentInit: async (
+    _: any,
+    args: TPaymentInitInput,
+    { currentUser, prisma }: TContext,
+  ) => {
+    await auth(prisma, currentUser, [ROLE.PATIENT]);
+
+    const parsedData = await Payment.validations.paymentInit.parseAsync(args);
+
+    const user = await prisma.user.findUnique({
+      where: { id: currentUser?.id },
+    });
+
+    let appointmentId: string;
+
+    if (parsedData.appointmentId) {
+      appointmentId = parsedData.appointmentId;
+    } else {
+      const lastAppointment = await prisma.appointment.findFirst({
+        where: {
+          patient: {
+            userId: user?.id,
+          },
+          payment: {
+            status: PAYMENT_STATUS.PENDING,
+          },
+          status: APPOINTMENT_STATUS.SCHEDULED,
+        },
+        orderBy: { createdAt: "desc" },
+        select: { id: true },
+      });
+
+      appointmentId = lastAppointment?.id || "";
+    }
+
+    if (!appointmentId) {
+      throw new AppError(status.NOT_FOUND, "Appointment not found");
+    }
+
+    const appointment = await prisma.appointment.findUnique({
+      where: { id: appointmentId },
+      select: {
+        id: true,
+        service: {
+          select: {
+            name: true,
+          },
+        },
+        doctor: {
+          select: {
+            user: {
+              select: {
+                firstName: true,
+                lastName: true,
+              },
+            },
+          },
+        },
+        payment: {
+          select: {
+            amount: true,
+          },
+        },
+      },
+    });
+
+    if (!appointment) {
+      throw new AppError(status.NOT_FOUND, "Appointment not found");
+    }
+
+    const paymentData = utils.getPaymentData({
+      name: user?.firstName + " " + user?.lastName,
+      email: user?.email,
+      phone: user?.phoneNumber,
+      address: user?.address,
+      appointmentId: appointment.id,
+      amount: appointment?.payment?.amount,
+      service:
+        appointment?.service?.name ||
+        appointment?.doctor?.user?.firstName +
+          " " +
+          appointment?.doctor?.user?.lastName,
+    });
+
+    const response = await axios({
+      method: "post",
+      url: config.ssl.sessionUrl,
+      data: paymentData,
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+
+    return response.data?.GatewayPageURL;
   },
 };
 
