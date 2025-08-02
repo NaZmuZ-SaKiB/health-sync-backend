75a7bdfd0e466b65274da20ec2b7e5277c04c8ce
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-02 15:46:41 +0600

    feat: refactor server setup to use express middleware and improve structure

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import bcrypt from "bcrypt";
-import { startStandaloneServer } from "@apollo/server/standalone";
+// import { startStandaloneServer } from "@apollo/server/standalone";
 import createApolloGraphqlServer from "./graphql";
 import { PrismaClient, ROLE } from "@prisma/client";
 import { TContext } from "./types";
 import { jwtHelpers } from "./utils/jwtHelper";
 import config from "./config";
+import express from "express";
+import cors from "cors";
+import { ApolloServer } from "@apollo/server";
+import bodyParser from "body-parser";
+import { expressMiddleware } from "@apollo/server/express4";
+import MainRouter from "./route";
 
-const prisma = new PrismaClient();
+export const prisma = new PrismaClient();
 
 const main = async () => {
-  const server = await createApolloGraphqlServer();
+  const app = express();
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+
+  const server = createApolloGraphqlServer() as ApolloServer<TContext>;
 
   // Seed Super Admin
   const isSuperAdminExist = await prisma.user.findFirst({
@@ -23,7 +34,7 @@ const main = async () => {
     try {
       const hashPassword = await bcrypt.hash(
         config.super_admin_password as string,
-        Number(config.bcrypt_salt_rounds)
+        Number(config.bcrypt_salt_rounds),
       );
 
       await prisma.user.create({
@@ -41,25 +52,54 @@ const main = async () => {
     }
   }
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-    context: async ({ req }): Promise<TContext> => {
-      const token = req.headers.authorization || "";
-      let currentUser = null;
-      if (token) {
-        currentUser = jwtHelpers.verifyToken(
-          token,
-          config.jwt.jwt_access_token_secret as string
-        );
-      }
-      return {
-        prisma,
-        currentUser,
-      };
-    },
+  await server.start();
+
+  app.use(
+    "/graphql",
+    expressMiddleware(server, {
+      context: async ({ req }): Promise<TContext> => {
+        const token = req.headers.authorization || "";
+        let currentUser = null;
+        if (token) {
+          currentUser = jwtHelpers.verifyToken(
+            token,
+            config.jwt.jwt_access_token_secret as string,
+          );
+        }
+        return {
+          prisma,
+          currentUser,
+        };
+      },
+    }),
+  );
+
+  // Application Routes
+  app.use("/api", MainRouter);
+
+  app.listen(config.port, () => {
+    console.log(`🚀 Server ready at http://localhost:${config.port}/graphql`);
   });
 
-  console.log(`🚀  Server ready at: ${url}`);
+  // const { url } = await startStandaloneServer(server, {
+  //   listen: { port: 4000 },
+  //   context: async ({ req }): Promise<TContext> => {
+  //     const token = req.headers.authorization || "";
+  //     let currentUser = null;
+  //     if (token) {
+  //       currentUser = jwtHelpers.verifyToken(
+  //         token,
+  //         config.jwt.jwt_access_token_secret as string,
+  //       );
+  //     }
+  //     return {
+  //       prisma,
+  //       currentUser,
+  //     };
+  //   },
+  // });
+
+  // console.log(`🚀  Server ready at: ${url}`);
 };
 
 main().catch((error) => {
