e375f6be6e88d9638ca41d6ba20f331c024e3195
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-04 22:16:24 +0600

    Replace icon URL field with iconId in specialty schema

--- src/graphql/specialty/specialty.validation.ts
+++ src/graphql/specialty/specialty.validation.ts
@@ -6,7 +6,7 @@ const create = z.object({
     .min(3, { message: "Specialty name must have min 3 chars." })
     .max(255),
   description: z.string().max(255).optional(),
-  icon: z.string().url({ message: "Invalid URL for icon." }).optional(),
+  iconId: z.string().optional(),
 });
 
 const update = create.partial().extend({
