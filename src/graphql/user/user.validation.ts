e84d0325dcfcb8bb5daa8125d8c57ece82cdc186
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-05 22:39:11 +0600

    Add password change validation schema

--- src/graphql/user/user.validation.ts
+++ src/graphql/user/user.validation.ts
@@ -76,10 +76,37 @@ const deleteAdmins = z.object({
   ids: z.array(z.string()).min(1, { message: "At least one ID is required" }),
 });
 
+const changePassword = z
+  .object({
+    password: z
+      .string()
+      .min(8, { message: "Password must be at least 8 characters long" })
+      .max(64, { message: "Password must be less than 64 characters" })
+      .regex(/[A-Z]/, {
+        message: "Password must contain at least one uppercase letter",
+      })
+      .regex(/[a-z]/, {
+        message: "Password must contain at least one lowercase letter",
+      })
+      .regex(/[0-9]/, { message: "Password must contain at least one number" })
+      .regex(/[!@#$%^&*()]/, {
+        message: "Password must contain at least one special character",
+      }),
+
+    confirmPassword: z
+      .string()
+      .min(1, { message: "Confirm password is required" }),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+
 export const validations = {
   create,
   signin,
   update,
   createAdmin,
   deleteAdmins,
+  changePassword,
 };
