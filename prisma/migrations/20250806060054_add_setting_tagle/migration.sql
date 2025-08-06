3ef5bd09252936e9b5e33f79c6e8fdaaceecb0c9
Nazmuz Sakib <sakibnazmuz01@gmail.com>
2025-08-06 22:37:45 +0600

    Add settings table with unique key constraint

---
+++ prisma/migrations/20250806060054_add_setting_tagle/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "public"."settings" (
+    "id" TEXT NOT NULL,
+    "key" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "settings_pkey" PRIMARY KEY ("id")
+);
